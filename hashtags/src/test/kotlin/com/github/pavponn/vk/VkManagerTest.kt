a3a3385b1f1a31b59d77097043450611d5efac1a
Pavel Ponomarev <pavponn@yandex.ru>
2020-10-04 00:37:36 +0300

    Add tests for VkManager and VkResponseParser

--- hashtags/src/test/kotlin/com/github/pavponn/vk/VkManagerTest.kt
+++ hashtags/src/test/kotlin/com/github/pavponn/vk/VkManagerTest.kt
@@ -1,4 +1,101 @@
 package com.github.pavponn.vk
 
+import com.github.pavponn.vk.client.VkClient
+import com.github.pavponn.vk.manager.VkManager
+import com.github.pavponn.vk.manager.VkManagerImpl
+import com.github.pavponn.vk.models.Response
+import com.github.pavponn.vk.models.Stats
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.mockito.ArgumentMatchers.*
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+import java.lang.IllegalArgumentException
+
+
+/**
+ * @author pavponn
+ */
 class VkManagerTest {
+    private lateinit var vkManager: VkManager
+
+    @Mock
+    private lateinit var vkClient: VkClient
+
+
+    @Before
+    public fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        vkManager = VkManagerImpl(vkClient)
+    }
+
+    @Test
+    public fun `should return valid stats for 1 hour`(): Unit = runBlocking {
+        val hashtag = "some_tag"
+        mockClientFunc(hashtag, Response(42, 42))
+        val result = vkManager.getHashTagStatsForNHoursFromNow(hashtag, 1)
+        Assert.assertEquals(result.size, 1)
+        Assert.assertEquals(result[0], Stats(hashtag, 42))
+    }
+
+    @Test
+    public fun `should return valid stats for multiple hours`(): Unit = runBlocking {
+        val hashtag = "another_tag"
+        mockClientFunc(hashtag, Response(2, 2))
+        val result = vkManager.getHashTagStatsForNHoursFromNow(hashtag, 4)
+        val elem = Stats(hashtag, 2)
+        Assert.assertEquals(result.size, 4)
+        Assert.assertEquals(result, listOf<Stats>(elem, elem, elem, elem))
+    }
+
+    @Test
+    public fun `should return stats with -1 counter if client return null`(): Unit = runBlocking {
+        val hashtag = "hashtag"
+        mockClientFunc(hashtag, null)
+        val res = vkManager.getHashTagStatsForNHoursFromNow(hashtag, 1)
+        Assert.assertEquals(res.size, 1)
+        Assert.assertEquals(res[0], Stats(hashtag, -1))
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    public fun `should throw if hashtag is empty`(): Unit = runBlocking {
+        val hashtag = ""
+        mockClientFunc(hashtag, Response(2, 2))
+        vkManager.getHashTagStatsForNHoursFromNow(hashtag, 5)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    public fun `should throw if hours param equals 0`(): Unit = runBlocking {
+        val hashtag = "tag"
+        mockClientFunc(hashtag, Response(2, 2))
+        vkManager.getHashTagStatsForNHoursFromNow(hashtag, 0)
+    }
+
+    @Test(expected = IllegalArgumentException::class)
+    public fun `should throw if hours var is negative`(): Unit = runBlocking {
+        val hashtag = "tag"
+        mockClientFunc(hashtag, Response(2, 2))
+        vkManager.getHashTagStatsForNHoursFromNow(hashtag, -1)
+    }
+
+
+    @Test(expected = IllegalArgumentException::class)
+    public fun `should throw if hours param is more than 24`(): Unit = runBlocking {
+        val hashtag = "tag"
+        mockClientFunc(hashtag, Response(2, 2))
+        vkManager.getHashTagStatsForNHoursFromNow(hashtag, 25)
+    }
+
+
+
+    private fun mockClientFunc(hashtag: String, response: Response?) = runBlocking {
+        Mockito
+            .`when`(vkClient.getStatByHashtagsForHourBeforeTime(safeEq(hashtag), anyLong(), anyLong()))
+            .thenReturn(response)
+    }
+
+    private fun <T : Any> safeEq(value: T): T = org.mockito.Matchers.eq(value) ?: value
 }
--- hashtags/src/test/kotlin/com/github/pavponn/vk/VkResponseParserTest.kt
+++ hashtags/src/test/kotlin/com/github/pavponn/vk/VkResponseParserTest.kt
@@ -1,4 +1,61 @@
 package com.github.pavponn.vk
 
+import com.github.pavponn.exceptions.ResponseParserException
+import com.github.pavponn.vk.parser.VkResponseParser
+import com.github.pavponn.vk.parser.VkResponseParserImpl
+import org.jetbrains.annotations.TestOnly
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+
+/**
+ * @author pavponn
+ */
 class VkResponseParserTest {
+    private lateinit var vkResponseParser: VkResponseParser
+
+    @Before
+    fun setUp() {
+        vkResponseParser = VkResponseParserImpl();
+    }
+
+    @Test
+    fun `should parse correct response`() {
+        val responseString = "{\"response\":{\"count\":20,\"total_count\":20}}"
+        val response = vkResponseParser.parse(responseString)
+        Assert.assertEquals(response.count, 20)
+        Assert.assertEquals(response.total_count, 20)
+    }
+
+    @Test
+    fun `should parse correct response and skip unknown keys`() {
+        val responseString = "{\"response\":{\"items\":[],\"count\":6,\"total_count\":7}}"
+        val response = vkResponseParser.parse(responseString)
+        Assert.assertEquals(response.count, 6)
+        Assert.assertEquals(response.total_count, 7)
+    }
+
+    @Test(expected = ResponseParserException::class)
+    fun `should throw when count field is not presented`() {
+        val responseString = "{\"response\":{\"total_count\":7}}"
+        vkResponseParser.parse(responseString)
+    }
+
+    @Test(expected = ResponseParserException::class)
+    fun `should throw when total_count field is not presented`() {
+        val responseString = "{\"response\":{\"count\":7}}"
+        vkResponseParser.parse(responseString)
+    }
+
+    @Test(expected = ResponseParserException::class)
+    fun `should throw when no field is presented`() {
+        val responseString = "{\"response\":{}}"
+        vkResponseParser.parse(responseString)
+    }
+
+    @Test(expected = ResponseParserException::class)
+    fun `should throw when response contains error`() {
+        val responseString = "{\"error\":{\"message\":\"some error\"}}"
+        vkResponseParser.parse(responseString)
+    }
 }

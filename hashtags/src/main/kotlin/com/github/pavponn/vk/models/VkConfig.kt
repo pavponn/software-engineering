81d77c07d9b5373837308d3452e551a6baeb9a50
Pavel Ponomarev <pavponn@yandex.ru>
2020-10-13 09:52:51 +0300

    Reformat file

--- hashtags/src/main/kotlin/com/github/pavponn/vk/models/VkConfig.kt
+++ hashtags/src/main/kotlin/com/github/pavponn/vk/models/VkConfig.kt
@@ -5,7 +5,10 @@ import java.time.Duration
 /**
  * @author pavponn
  */
-data class VkConfig(val accessToken: String, val urlConfig: UrlConfig, val version: Version, val timeout: Duration = Duration.ofMillis(500))
+data class VkConfig(val accessToken: String,
+                    val urlConfig: UrlConfig,
+                    val version: Version,
+                    val timeout: Duration = Duration.ofMillis(500))
 
 data class Version(val major: Int, val minor: Int)
 
